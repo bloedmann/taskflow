381e1c1e81eafb9b52712907695579a9b4b4c418
Yuecel <71930662+bloedmann@users.noreply.github.com>
2026-03-15 19:37:02 +0100

    Update __init__.py comments

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
+# Tasks-Blueprint mit Präfix /tasks
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+# Importiert die zugehörigen Routen
 from . import routes
