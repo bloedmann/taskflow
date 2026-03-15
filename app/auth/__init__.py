560c13c3348f929d32f4477b9de5c924411ab0ae
Yuecel <71930662+bloedmann@users.noreply.github.com>
2026-03-15 19:33:25 +0100

    Update __init__.py comments

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
+# Auth-Blueprint mit Präfix /auth
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
+# Importiert die zugehörigen Routen
 from . import routes
