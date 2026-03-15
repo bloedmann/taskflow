7b293d18d67f973400b5f5198a886c9b1c42eb87
Yuecel <71930662+bloedmann@users.noreply.github.com>
2026-03-15 19:33:58 +0100

    Update forms.py comments

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
+# Formular für die Registrierung
 class RegisterForm(FlaskForm):
     username = StringField("Benutzername", validators=[DataRequired(), Length(min=3, max=80)])
     email = StringField("E-Mail", validators=[DataRequired(), Email(), Length(max=120)])
@@ -14,6 +15,7 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Registrieren")
 
 
+# Formular für den Login
 class LoginForm(FlaskForm):
     email = StringField("E-Mail", validators=[DataRequired(), Email()])
     password = PasswordField("Passwort", validators=[DataRequired()])
